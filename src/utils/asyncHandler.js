59238e4cc7df033efcf9a973e86a90a8349810d4
Himanshu Mandowra <himanshu.toppr@gmail.com>
2026-03-01 14:58:53 +0530

    Util code and files...
    1 March 2026...

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,24 @@
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        promise.relove(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+
+
+
+
+export {asyncHandler}
+
+
+
+// const asyncHandler = () => async(req, res, next) => {
+//     try {
+//         await fin(req, res, next)
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
