61f0e99ce233abb910b6af489f8b2612d6e04fbe
Himanshu Mandowra <himanshu.toppr@gmail.com>
2026-05-06 20:47:22 +0530

    Restructured project into frontend and backend monorepo

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "cors"
 import cookieParser from "cookie-parser";
+import resumeRoutes from "./routes/resume.routes.js";
+import swaggerUi from "swagger-ui-express";
+import swaggerSpec from "./swagger.js";
 
 const app = express()
 
@@ -8,6 +11,8 @@ app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
@@ -18,5 +23,6 @@ app.use(cookieParser())
 import userRouter from './routes/user.routes.js'
 
 app.use("/api/v1/users", userRouter)
+app.use("/api/resume", resumeRoutes);
 
 export { app }
